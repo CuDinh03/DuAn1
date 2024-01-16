8eae58dde0d6b7b1ab87d918d71e601250ebbb96
CuDinh03 <dinhhuycu0305@gmail.com>
2024-01-16 22:05:32 +0700

    tao file moi

---
+++ src/duan1_nhom1/demogit.java
@@ -0,0 +1,9 @@
+package duan1_nhom1;
+
+public class demogit {
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        System.out.println("hello");
+    }
+}
