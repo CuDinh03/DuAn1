16774da8b2ba8d83ab92c27c583c8b152a78e7a8
Lê Anh Tuấn <114102305+anhtuando19@users.noreply.github.com>
2024-01-17 22:15:25 +0700

    update

--- src/duan1_nhom1/model/Hang.java
+++ src/duan1_nhom1/model/Hang.java
@@ -4,10 +4,90 @@
  */
 package duan1_nhom1.model;
 
+import java.util.Date;
+import java.util.UUID;
+
 /**
  *
  * @author maccuacu
  */
 public class Hang {
+    private UUID id;
+    private String ma;
+    private String ten;
+    private String moTa;
+    private Date ngayTao;
+    private Date ngaySua;
+    private boolean trangThai;
+
+    public Hang() {
+    }
+
+    public Hang(UUID id, String ma, String ten, String moTa, Date ngayTao, Date ngaySua, boolean trangThai) {
+        this.id = id;
+        this.ma = ma;
+        this.ten = ten;
+        this.moTa = moTa;
+        this.ngayTao = ngayTao;
+        this.ngaySua = ngaySua;
+        this.trangThai = trangThai;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getMa() {
+        return ma;
+    }
+
+    public void setMa(String ma) {
+        this.ma = ma;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public String getMoTa() {
+        return moTa;
+    }
+
+    public void setMoTa(String moTa) {
+        this.moTa = moTa;
+    }
+
+    public Date getNgayTao() {
+        return ngayTao;
+    }
+
+    public void setNgayTao(Date ngayTao) {
+        this.ngayTao = ngayTao;
+    }
+
+    public Date getNgaySua() {
+        return ngaySua;
+    }
+
+    public void setNgaySua(Date ngaySua) {
+        this.ngaySua = ngaySua;
+    }
+
+    public boolean isTrangThai() {
+        return trangThai;
+    }
+
+    public void setTrangThai(boolean trangThai) {
+        this.trangThai = trangThai;
+    }
+    
     
 }
